84d4b4145c4c34b4f0340aa449da4d1c29d3c95d
annabelhuber <annabelhuber123@gmail.com>
2026-06-11 17:09:37 -0400

    changed default page to login

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -93,18 +93,12 @@ export function LoginForm({
                 {isLoading ? "Logging in..." : "Login"}
               </Button>
             </div>
-            <div className="mt-4 text-center text-sm">
-              Don&apos;t have an account?{" "}
-              <Link
-                href="/auth/sign-up"
-                className="underline underline-offset-4"
-              >
-                Sign up
-              </Link>
-            </div>
           </form>
         </CardContent>
       </Card>
+      <Button asChild variant="outline" className="w-full">
+        <Link href="/auth/sign-up">New here? Sign up now</Link>
+      </Button>
     </div>
   );
 }
